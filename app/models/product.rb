50d171f37fe4893e15e364eba35b93c7bbc4e4dc
Serhii Trushko <TrushkoSergey2@gmail.com>
2020-04-06 00:05:29 +0300

    Add booton

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -8,4 +10,14 @@ class Product < ApplicationRecord
     Product.order(:updated_at).last
   end
 
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'сущестевуют товарные позиции')
+      return false
+    end
+  end
 end
