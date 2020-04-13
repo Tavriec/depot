a849fb8ab2528503963a7b6f66acf41c1f8084b7
Serhii Trushko <TrushkoSergey2@gmail.com>
2020-04-13 21:23:43 +0300

    Add scaffold Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_121712) do
+ActiveRecord::Schema.define(version: 2020_04_13_181303) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -23,9 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_121712) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "quantity", default: 1
-    t.integer "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
-    t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
@@ -48,6 +46,5 @@ ActiveRecord::Schema.define(version: 2020_04_13_121712) do
   end
 
   add_foreign_key "line_items", "carts"
-  add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
 end
