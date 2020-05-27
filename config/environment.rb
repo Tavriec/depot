3a8fa93d2a89ff3fb87bf9dbd93ed489cbed8e80
Serhii Trushko <TrushkoSergey2@gmail.com>
2020-05-27 11:52:39 +0300

    update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'example.com',
+#   user_name:            'TrushkoSergey2',
+#   password:             'Nokia_n72',
+#   authentication:       'plain',
+#   enable_starttls_auto: true  
+# }
